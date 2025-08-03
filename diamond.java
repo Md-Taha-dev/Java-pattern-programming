929268621b6241f836c49c689a6dd90c31d39a56
Md Taha <tahamohammed.ece@gmail.com>
2025-08-03 14:59:12 +0530

    Update diamond.java

--- diamond.java
+++ diamond.java
@@ -1,6 +1,6 @@
 package taha;
 import java.util.Scanner;
-public class patterns {
+public class diamond {
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);	
 		int n=sc.nextInt();
